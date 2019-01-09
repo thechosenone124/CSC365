214a44e24872c9b4da52df1f272708a690d4c86c
btan03 <thechosenone124@gmail.com>
2019-01-09 13:17:12 -0800

    created the students tostring

---
+++ students.py
@@ -0,0 +1,7 @@
+grades = ["kindergarten", "1st grade", "2nd grade", "3rd grade", "4th grade", "5th grade", "6th grade"]
+def printStudent (StLastName, StFirstName, Grade, Classroom, Bus, GPA, TLastName, TFirstName) : 
+   return (StFirstName + " " + StLastName + " who takes bus route " + 
+      Bus + ", is a " + grades[Grade] + " student assigned to the class of "
+      + TFirstName + " " + TLastName + " in the classroom " + Classroom 
+      + ". He has a GPA of " + GPA + ".")
+   
