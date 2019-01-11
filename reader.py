327fc672efb6b345d1b18ae78f72539cdc902759
btan03 <thechosenone124@gmail.com>
2019-01-11 13:27:45 -0800

    created the csv reader

---
+++ reader.py
@@ -0,0 +1,24 @@
+import csv
+
+StLastName = 0
+StFirstName = 1
+Grade = 2
+Classroom = 3
+Bus = 4
+GPA = 5
+TLastName = 6
+TFirstName = 7
+
+def getStudents():
+   with open('students.txt', 'rb') as csvfile:
+      output = list()
+      reader = csv.reader(csvfile, delimiter=',')
+      for row in reader:
+         output.append(row)
+      return output
+
+
+for row in getStudents():
+   print ", ".join(row)
+      
+      
