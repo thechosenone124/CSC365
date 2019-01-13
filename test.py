2738adca1804cf4951fe6e7888863d7c2ab4e65a
Bryan <thechosenone124@gmail.com>
2019-01-13 04:00:14 -0800

    updated tests, still not running

--- test.py
+++ test.py
@@ -1,21 +1,22 @@
-from unittest import TestCase
+import unittest
 from unittest.mock import patch
+import io
 import schoolsearch
 
 class TestClass(unittest.TestCase):
    def runTest(self, given_answer, expected_out):
-        with patch('builtins.input', return_value=given_answer), patch('sys.stdout', new=StringIO()) as fake_out:
+        with patch('builtins.input', return_value=given_answer), patch('sys.stdout', new=io.StringIO()) as fake_out:
             schoolsearch.main()
             self.assertEqual(fake_out.getvalue().strip(), expected_out)
             
    def test_last_name_search(self):
-       self.runTest("S: CORONADO", "CORONADO, DIMPLE, 6, 102, KERBS, BENITO")
+       self.runTest("S: CORONADO\nQ", "CORONADO, DIMPLE, 6, 102, KERBS, BENITO")
 
    def test_last_name_search_bus(self):
-       self.runTest("S: CORONADO B", "CORONADO, DIMPLE, 52")
+       self.runTest("S: CORONADO B\nQ", "CORONADO, DIMPLE, 52")
 
    def test_last_name_search_bus(self):
-       self.runTest("I", "1: 2\n" +
+       self.runTest("I\nQ", "1: 2\n" +
             "2: 13\n" +
             "3: 9\n" +
             "4: 15\n" +
@@ -23,7 +24,6 @@ class TestClass(unittest.TestCase):
             "6: 21\n")
 
 if __name__ == '__main__':
-   suite = unittest.TestLoader().loadTestsFromTestCase(TestStringMethods)
-   unittest.TextTestRunner(verbosity=2).run(suite)
+   unittest.main()
                  
 
